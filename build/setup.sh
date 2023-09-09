23ad57011fa94b9afccb1caf14f736b4d179f662
JamesMCL44 <epicquarters@gmail.com>
2023-09-09 16:53:03 +0800

    Update

--- build/setup.sh
+++ build/setup.sh
@@ -21,7 +21,6 @@ echo
 apt-get -y install htop nano net-tools zip unzip openssh-server
 echo
 echo "deb http://security.ubuntu.com/ubuntu bionic-security main" | sudo tee -a /etc/apt/sources.list.d/bionic.list
-apt-get upgrade -y
 apt-get -y update
 apt-get install -y -f
 apt-cache policy libssl1.0-dev
@@ -37,13 +36,13 @@ rm /etc/nginx/conf.d/*
 find / -name *.ini
 
 # Change max execution time to 180 seconds
-sed -ri 's/(max_execution_time =) ([2-9]+)/\1 180/' /etc/php/8.2/fpm/php.ini
+sed -ri 's/(max_execution_time =) ([2-9]+)/\1 180/' /etc/php/${PHP_VERSION}/fpm/php.ini
 
 # Max memory to allocate for each php-fpm process
-sed -ri 's/(memory_limit =) ([0-9]+)/\1 1024/' /etc/php/8.2/fpm/php.ini
+sed -ri 's/(memory_limit =) ([0-9]+)/\1 1024/' /etc/php/${PHP_VERSION}/fpm/php.ini
 
 # Set the timezone
-sed -ri "s@^;date\.timezone\s+.*@date\.timezone=${TZ}@" /etc/php/8.2/fpm/php.ini
+sed -ri "s@^;date\.timezone\s+.*@date\.timezone=${TZ}@" /etc/php/${PHP_VERSION}/fpm/php.ini
 
 # Install ioncube loader
 cd /tmp && curl -o ioncube.zip http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.zip && \
@@ -57,9 +56,9 @@ cd /tmp && curl -o ioncube.zip http://downloads3.ioncube.com/loader_downloads/io
     cp /tmp/ioncube/ioncube_loader_lin_8.2.so /usr/lib/php/ioncube/. && \
     echo "Step 5" && \
     echo "zend_extension = /usr/lib/php/ioncube/ioncube_loader_lin_8.2.so" \
-    > /etc/php/8.2/fpm/conf.d/00-ioncube.ini && \
+    > /etc/php/${PHP_VERSION}/fpm/conf.d/00-ioncube.ini && \
     echo "Step 6" && \
-    cp /etc/php/8.2/fpm/conf.d/00-ioncube.ini /etc/php/8.2/cli/conf.d/. && \
+    cp /etc/php/${PHP_VERSION}/fpm/conf.d/00-ioncube.ini /etc/php/${PHP_VERSION}/cli/conf.d/. && \
     echo "Step 7"
 
 # Install Dockerize
